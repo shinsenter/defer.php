e925a979cae3bf0ad5b0a5a7b797e3fbef21d6d4
shinsenter <2082119+shinsenter@users.noreply.github.com>
2019-03-29 17:59:09 +0900

    Add symfony http component, improve css

--- defer.php
+++ defer.php
@@ -25,6 +25,7 @@ if (!class_exists('shinsenter\Defer')) {
         require_once $baseDir . '/src/Helpers/JsMin.php';
         require_once $baseDir . '/src/DeferException.php';
         require_once $baseDir . '/src/DeferCache.php';
+        require_once $baseDir . '/src/DeferHttpRequest.php';
         require_once $baseDir . '/src/DeferInterface.php';
         require_once $baseDir . '/src/DeferOptions.php';
         require_once $baseDir . '/src/DeferParser.php';
